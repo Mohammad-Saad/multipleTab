2bd4eab91127470fbe95d9de7d37797265347524
Mohammad-Saad <saad.ash29@gmail.com>
2015-05-01 03:35:51 -0700

    Initial Commit

---
+++ multipleTab/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  multipleTab
+//
+//  Created by Hassaan Masood on 5/1/15.
+//  Copyright (c) 2015 Szabist. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
